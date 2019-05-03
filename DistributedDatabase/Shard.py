cd45266ae447f55a375d9eec1aabc23171be5689
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-05-03 17:22:01 +0200

    fixed bug in pub sub database

--- DistributedDatabase/Shard.py
+++ DistributedDatabase/Shard.py
@@ -7,6 +7,9 @@ Created on Thu Apr 18 15:22:33 2019
 import zmq
 import sys
 import pymysql as PyMySQL
+import threading
+from queue import Queue
+import time
 
 # Open database connection
 db = PyMySQL.connect("localhost","testuser","123456","Usersdb" )
@@ -14,11 +17,8 @@ db = PyMySQL.connect("localhost","testuser","123456","Usersdb" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-#### be a server when a client wants a task done 
-#### if sign up make sure the username is new and not on the db
-#### if login check for the paswword
 
-
+#------------------ initial configuration -----------------------#
 port=int(sys.argv[1])
 portPub = int(sys.argv[2])
 firstPortSecondShard = int (sys.argv[3])
@@ -44,64 +44,81 @@ contextSub = zmq.Context()
 socketSub = contextSub.socket(zmq.SUB)
 for i in range (len(mysub)): 
     socketSub.connect ("tcp://localhost:%s" % mysub[i])
-    topicfilter = "Insert"
+    topicfilter = "INSERT"
     socketSub.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
 
-while True:
-    message = socket.recv()
-    print ("Received request: ", message)    
-    msg = str(message,'utf-8')
-    query = msg.split()
-    print(query)
-    if (query[0] =='1'): # Sign Up
-        sqlcheck = "SELECT * FROM USERS where Username= %s"
-        cursor.execute(sqlcheck,(query[1],))
-        result = cursor.fetchall()
-        print(result)
-        if len(result)>0:
-            messageToSend = "Can't sign Up with this Username , please choose another One"
-        else:            
-            sql ="INSERT INTO USERS (UserName,UserPassword,Email) VALUES ( %s , %s ,%s )"
+def ServerPub():
+    # this function is responsible for executing client command
+    # and if sign up (new insetion) publish to other shards immediatly
+    while True:
+        message = socket.recv()
+        print ("Received request: ", message)    
+        msg = str(message,'utf-8')
+        query = msg.split()
+        print(query)
+        if (query[0] =='1'): # Sign Up
+            sqlcheck = "SELECT * FROM USERS where Username= %s"
+            cursor.execute(sqlcheck,(query[1],))
+            result = cursor.fetchall()
+            print(result)
+            if len(result)>0:
+                messageToSend = "Can't sign Up with this Username , please choose another One"
+            else:            
+                sql ="INSERT INTO USERS (UserName,UserPassword,Email) VALUES ( %s , %s ,%s )"
+                try:
+                   # Execute the SQL command
+                    cursor.execute(sql,(query[1],query[2],query[3],))
+                    # Commit your changes in the database
+                    db.commit()
+                    ### then publish that if it is an insertion
+                    #topic = "Insert"
+                    messagedata = "INSERT INTO USERS (UserName,UserPassword,Email) VALUES ( '"+query[1]+"' , '"+query[2]+"' , '" +query[3] + "' )"
+                    socketPub.send_string("%s" % (messagedata))
+                    messageToSend = "Signed in Sucessfully"
+                except:
+                   messageToSend = "Couldn't connect .. try again later"
+                   print(messageToSend)
+                   # Rollback in case there is any error
+                   db.rollback()
+                   
+        else: #log in
+            print("Log in")
             try:
-               # Execute the SQL command
-               cursor.execute(sql,(query[1],query[2],query[3],))
-               # Commit your changes in the database
-               db.commit()
-               ### then publish that if it is an insertion
-               topic = "Insert"
-               messagedata = "INSERT INTO USERS (UserName,UserPassword,Email) VALUES ( "+query[1]+" , "+query[2]+" , " +query[3] + " )"
-               socket.send_string("%d %d" % (topic, messagedata))
+                sql = "SELECT * FROM USERS where Username= %s and UserPassword = %s"
+                cursor.execute(sql,(query[1],query[2],))
+                result = cursor.fetchall()
+                if len(result) ==0:
+                    messageToSend = "Username or password is incorrect, Please try again"
+                else:
+                    messageToSend="Logged in Sucessfully"
             except:
-               # Rollback in case there is any error
-               db.rollback()
-            messageToSend = "Signed in Sucessfully"
-    else: #log in
-        print("Log in")
-        sql = "SELECT * FROM USERS where Username= %s and UserPassword = %s"
-        cursor.execute(sql,(query[1],query[2],))
-        result = cursor.fetchall()
-        if len(result) ==0:
-            messageToSend = "Username or password is incorrect, Please try again"
-        else:
-            messageToSend="Logged in Sucessfully"
+                messageToSend = "Couldn't connect .. try again later"
+            
+        socket.send_string(messageToSend)
+
+
+def Sub():
+    while True:
+        messagedata = socketSub.recv()
+        print('Subscriber on .. inserting in db query',messagedata)
+        try:
+       # Execute the SQL command
+            cursor.execute(messagedata)
+       # Commit your changes in the database
+            db.commit()
         
-    socket.send_string(messageToSend)
+        except:
+            print("Already inserted (may be) ..")
+            
 
+server_pub = threading.Thread(target=ServerPub)
+sub = threading.Thread(target=Sub)
 
+server_pub.start()
+sub.start()
 
-### subscribe to other machines to get new updates
-    string = socketSub.recv()
-    topic, messagedata = string.split()
-    print (messagedata) ## sql query to be executed
-    try:
-           # Execute the SQL command
-           cursor.execute(sql,(query[1],query[2],query[3],))
-           # Commit your changes in the database
-           db.commit()
-    except:
-           # Rollback in case there is any error
-           db.rollback()
-
+server_pub.join()
+sub.join()
 
 # disconnect from server
 #db.close()
--- DistributedDatabase/runshards.sh
+++ DistributedDatabase/runshards.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-python shard.py 5000 5030 5035 5040 &
-python shard.py 5001 5031 5035 5040 &
-python shard.py 5002 5032 5035 5040 &
+python shard.py 5000 5030 5035 5045 &
+python shard.py 5001 5031 5035 5045 &
+python shard.py 5002 5032 5035 5045 &
 
-python shard.py 5005 5035 5030 5040 &
-python shard.py 5006 5036 5030 5040 &
-python shard.py 5007 5037 5030 5040 &
+python shard.py 5005 5035 5030 5045 &
+python shard.py 5006 5036 5030 5045 &
+python shard.py 5007 5037 5030 5045 &
 
-python shard.py 5010 5040 5030 5035 &
-python shard.py 5011 5041 5030 5035 &
-python shard.py 5012 5042 5030 5035 &
+python shard.py 5010 5045 5030 5035 &
+python shard.py 5011 5046 5030 5035 &
+python shard.py 5012 5047 5030 5035 &
 
 python clientdb.py 
