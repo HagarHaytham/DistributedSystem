9c16e48afe3f844677b68fa54a09f86b251ebbe7
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-05-06 10:48:14 +0200

    editing

--- Nodes/Nodes_client.py
+++ Nodes/Nodes_client.py
@@ -145,7 +145,7 @@ def replicate(context, port):
     return
 
 ########################################################## 
-def main(aliveP, upldP, successP, dwnldP, replServerP)
+def main(aliveP, upldP, successP, dwnldP, replServerP):
 
     context = zmq.Context()
     success = initConn(context, successP)    
@@ -160,7 +160,7 @@ def main(aliveP, upldP, successP, dwnldP, replServerP)
     replicationThread.start()
 
     t1.start()
-    t2.start()
+    # t2.start()
 
     return
 
