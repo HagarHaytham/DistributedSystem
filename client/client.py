6eec99c232177e3fdf20ec2072a524fc0268eed2
HaneenWally <livetour1d>
2019-05-06 13:50:38 +0200

    fix merge

--- client/client.py
+++ client/client.py
@@ -103,6 +103,9 @@ def main(IPS,ipServer,dataIps):
            
             if(read == '1'):
              #  Get the port from server
+                socketID.send_string(choice)
+                reply = socketID.recv_string()
+
                 dataNodeSocket = initUplNodePort(context,reply,dataIps)
 
                 #uploading happens
