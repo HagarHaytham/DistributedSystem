f53f9b933e439ae8dcdc8a3a1ce02b3bb03dae03
Mary_N <marynader256@gmail.com>
2019-05-06 11:49:17 +0200

    IPs added and taking arguments

--- Nodes/nodereplicate.py
+++ Nodes/nodereplicate.py
@@ -5,69 +5,75 @@ Created on Sat May  4 00:28:29 2019
 @author: Mary
 """
 import zmq
-import socket
 import sys
 import time
 
-def replicate():
-    port = "1100"
+def replicate(port):
+    #port = "1100"
     if len(sys.argv) > 1:
         port =  sys.argv[1]
         int(port)
-    context = zmq.Context()
-    rSocket = context.socket(zmq.REP)
-    rSocket.bind("tcp://*:%s" % port)
-    rOrS=rSocket.recv_string()
-    dst1 = ""
-    dst2 = ""
-    file = ""
-    rSocket.send_string("node: your request recieved")
-    if(rOrS == "s"):
-        dst1 = rSocket.recv_string()
-        #time.sleep(5)
-        rSocket.send_string("node: dst1 recieved")
-
-        dst2 = rSocket.recv_string()
-        rSocket.send_string("node: dst2 recieved")
-
-        file = rSocket.recv_string()
-        rSocket.send_string("node: file recieved")
-        
-        print(rSocket.recv_string())
-        
-        time.sleep(5)
-        openedFile = open(file,'rb')
-        readFile = openedFile.read()
-        if(dst1 != ""):
-            dstSocket1 = context.socket(zmq.REQ)
-            dstSocket1.connect("tcp://localhost:%s" % dst1)
+    while(1):
+        context = zmq.Context()
+        rSocket = context.socket(zmq.REP)
+        rSocket.bind("tcp://*:%s" % port)
+        rOrS=rSocket.recv_string()
+        dIP1 = ""
+        dst1 = ""
+        dIP2 = ""
+        dst2 = ""
+        file = ""
+        rSocket.send_string("node: your request recieved")
+        if(rOrS == "s"):
+            dIP1 = rSocket.recv_string()
+            rSocket.send_string("node: IP1 recieved")
+            dst1 = rSocket.recv_string()
+            rSocket.send_string("node: dst1 recieved")
+            
+            dIP2 = rSocket.recv_string()
+            rSocket.send_string("node: IP2 recieved")
+            dst2 = rSocket.recv_string()
+            rSocket.send_string("node: dst2 recieved")
     
-            print("sending first copy..." )
-            dstSocket1.send(readFile)
+            file = rSocket.recv_string()
+            rSocket.send_string("node: file recieved")
             
-        if(dst2 != ""):
-            dstSocket2 = context.socket(zmq.REQ)
-            dstSocket2.connect("tcp://localhost:%s" % dst2) 
+            print(rSocket.recv_string())
+            print("Hey I am sending" , file)
+            time.sleep(5)
+            openedFile = open(file,'rb')
+            readFile = openedFile.read()
+            if(dst1 != ""):
+                dstSocket1 = context.socket(zmq.REQ)
+                dstSocket1.connect("tcp://%s:%s" % (dIP1,dst1))
+        
+                print("sending first copy..." )
+                dstSocket1.send(readFile)
+                dstSocket1.close()
+            if(dst2 != ""):
+                dstSocket2 = context.socket(zmq.REQ)
+                dstSocket2.connect("tcp:///%s:%s" % (dIP2,dst2)) 
+                
+                print("sending second copy..." )
+                dstSocket2.send(readFile)
+                dstSocket2.close()
+            openedFile.close()
+            rSocket.send_string("node: Done replicating")
+            rSocket.close()
+        elif(rOrS == "r"):
+            print("replicating...")
+            file = rSocket.recv_string()
+            rSocket.send_string("rep node: file name recieved")
+    
+            recFile = rSocket.recv()
+            print("Done replication")
+            openedFile = open(file,'wb')
+            openedFile.write(recFile)
+            openedFile.close()
+            rSocket.close()
             
-            print("sending second copy..." )
-            dstSocket2.send(readFile)
-            
-        openedFile.close()
-        rSocket.send_string("node: Done replicating")
         
-    elif(rOrS == "r"):
-        print("replicating...")
-        file = rSocket.recv_string()
-        rSocket.send_string("rep node: file name recieved")
-
-        recFile = rSocket.recv()
-        
-        openedFile = open(file,'wb')
-        openedFile.write(recFile)
-        openedFile.close()
-        
-        
-replicate()
+#replicate(6000)
 
     
     
